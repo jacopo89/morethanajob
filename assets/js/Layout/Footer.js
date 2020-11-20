9a57486d7420e513f4b4988cfaacc3c3987a3311
jacop <jacopotrapani89@gmail.com>
2020-11-21 00:27:07 +0100

    sistemato il footer

--- assets/js/Layout/Footer.js
+++ assets/js/Layout/Footer.js
@@ -4,12 +4,12 @@ import React from "react";
 
 export default function Footer() {
     return (<div style={{width:"100%", backgroundColor:bordeaux, alignItems:"center", display:"flex", flexDirection:"column", padding:50}}>
-        <div style={{display: "flex", width:400, justifyContent: "space-around"}}>
+        <div style={{display: "flex", width:250, justifyContent: "space-around"}}>
             <a target="_blank" href="http://facebook.com/morethanajob"><Icon style={{color:"white"}} icon="facebook-square" size="3x"/></a>
             <a target="_blank" href="http://www.linkedin.com/company/morethanajobeni/"><Icon style={{color:"white"}} icon="linkedin-square" size="3x"/></a>
             <a target="_blank" href="http://twitter.com/than_job"><Icon style={{color:"white"}} icon="twitter-square" size="3x" /></a> </div>
         <p style={{color:"white"}}><a style={{color:"whitesmoke"}} href="http://www.enicbcmed.eu/projects/morethanajob">http://www.enicbcmed.eu/projects/morethanajob</a></p>
-        <div>
+        <div style={{marginTop:40}}>
             <span style={{color:"white", fontSize:9}}>“This website has been produced with the financial assistance of the European Union under the ENI CBC Mediterranean Sea Basin Programme. The contents of this document are the sole responsibility of CESIE and can under no circumstances be regarded as reflecting the position of the European Union or the Programme management structures.
 
             The 2014 2020 ENI CBC Mediterranean Sea Basin Programme is a multilateral Cross Border Cooperation ( initiative funded by the European Neighbourhood Instrument - ENI). The Programme objective is to foster fair, equitable and sustainable economic, social and territorial development, which may advance cross b order integration and valorise participating countries’ territories and values. The following 13 countries participate in the Programme: Cyprus, Egypt, France, Greece, Israel, Italy, Jordan, Lebanon, Malta, Palestine, Portugal, Spain, Tunisia. The Managing Authority (MA) is the Autonomous Region of Sardinia (Italy). Official Programme languages are Arabic, English and French. For more information, please visit:</span>
