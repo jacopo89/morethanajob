b494438230a0a81898bfb74822a9939f901a9840
jacopo <jacopotrapani89@gmail.com>
2021-05-09 18:11:22 +0200

    fix mail

--- assets/js/Functions/Profile.js
+++ assets/js/Functions/Profile.js
@@ -4,7 +4,9 @@ export function getProfileLanguageElements(user) {
 
     const {language} = useSelector(state=>state);
 
-    const description = (user && user.language === language && user.localLanguageDescription!==null && user.localLanguageDescription.length!==0) ? user && user.localDescription  : user && user.description ;
+    console.log(language);
+    console.log(user);
+    const description = (user && user.language === language && user.localLanguageDescription!==null && user.localLanguageDescription.length!==0) ? user && user.localLanguageDescription  : user && user.description ;
 
     return {
         description: description
